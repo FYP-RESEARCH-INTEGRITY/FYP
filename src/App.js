f2954f8f989f40b3c59543d97995913239f91432
Veronica <vamokwandoh@st.ug.edu.gh>
2024-07-19 15:31:57 +0000

    signin

--- src/App.js
+++ src/App.js
@@ -1,32 +1,47 @@
-import './App.css';
-import Signup from './Pages/Signup';
-import Signin from './Pages/Signin';
-import Upload from './Pages/Upload';
+import "./App.css";
+import Signup from "./Pages/Signup";
+import Signin from "./Pages/Signin";
+import Upload from "./Pages/Upload";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 import {
   createBrowserRouter,
   createRoutesFromElements,
-
+  Navigate,
   Route,
   RouterProvider,
+  useNavigate,
 } from "react-router-dom";
-import Layout from './Layout';
-
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path='/' element={<Layout />}>
-      <Route index element={<Signup />} />
-      <Route path="/Signin" element={<Signin />} />
-      <Route path="/Upload" element={<Upload />} />
-    </Route>
-  )
-);
-
+import Layout from "./Layout";
+import { useEffect, useState } from "react";
+import { auth } from "./Pages/firebase";
 
 function App() {
+  const [user, setUser] = useState();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      setUser(user);
+    });
+  });
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Layout />}>
+        <Route index element={user ? <Navigate to={"/Upload"} /> : <Signup /> } />
+        {/* <Route  path="/Signup" element={user ? <Navigate to={"/Upload"} /> : <Signup /> } /> */}
+        <Route path="/Signin" element={<Signin />} />
+        <Route path="/Upload" element={<Upload />} />
+      </Route>,
+    ),
+  );
+
   return (
-    <RouterProvider router={router} />
+    <>
+      <RouterProvider router={router} />
+      <ToastContainer />
+    </>
   );
 }
 
