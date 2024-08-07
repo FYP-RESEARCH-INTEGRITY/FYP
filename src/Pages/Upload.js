dedb4461bf463e78e5cf52b4b659d9f695313d9d
edem8 <eddieblack159@gmail.com>
2024-08-07 11:06:44 +0000

    chore: request clean up

--- src/Pages/Upload.js
+++ src/Pages/Upload.js
@@ -36,6 +36,7 @@ const UploadPage = () => {
     })
       .then(response => {
         if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
+        console.log(response.json())
         return response.json();
       });
 
