acb0fc82a6ad88e8d8f18297d5a702a5e6f9035b
Veronica <vamokwandoh@st.ug.edu.gh>
2024-08-18 21:06:33 +0000

    changes

---
+++ src/Pages/Signup.js
@@ -0,0 +1,155 @@
+import { useState,useEffect } from 'react';
+import { toast } from 'sonner'
+import {
+  createUserWithEmailAndPassword,
+  auth,
+  provider,
+  signInWithPopup,
+  db
+} from "../Services/firebase";
+import { FcGoogle } from "react-icons/fc";
+import { useNavigate } from "react-router-dom";
+
+
+
+
+
+function SignUp() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("")
+  const navigate = useNavigate();
+
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(user => {
+      if (user) {
+        navigate("/Upload");
+      }
+    });
+    return () => unsubscribe();
+  }, [navigate]);
+
+
+  const handleSignupWithEmailAndPassword = async (e) => {
+    e.preventDefault();
+    if (password !== confirmPassword) {
+      toast.error("Passwords do not match", {
+        position: "bottom-center",
+      });
+      return;
+    }
+    try {
+      await createUserWithEmailAndPassword(auth, email, password);
+      
+      toast.success("Registration successful");
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+
+  const handleGoogleSignIn = async () => {
+    try {
+      await signInWithPopup(auth, provider);
+     toast.success("Registration successful")
+    } catch (error) {
+      toast.error(`${error.message}`);
+    }
+  };
+
+
+
+  return (
+    <div className="max-h-[350px] bg-[#32324d] px-8 py-12 text-white">
+      <div className="logo">CITESCOUT</div>
+      <div className="flex">
+        <div className="mt-8">
+          <h1 className="text-[32px] font-semibold">Sign Up</h1>
+          <br />
+          <p className="max-w-[45ch]">
+            CiteScout is a community dedicated to maintaining academic integrity.
+            It offers a free platform for easy citation verification, plagiarism detection,
+            and academic misconduct prevention. It simplifies research processes with automated
+            checks and promotes transparency among peers.</p>
+          <p className="max-w-[45ch] mt-4">
+            CiteScout is a community dedicated to maintaining academic integrity. It offers a free platform for easy citation verification, plagiarism detection, and academic misconduct prevention. It simplifies research processes with automated checks and promotes transparency among peers.
+          </p>
+        </div>
+        <div>
+          <img
+            className="img"
+            src="https://res.cloudinary.com/dsoqjlpxd/image/upload/v1712764240/Saly-1_xyu5bh.png"
+            alt="Sign Up Illustration"
+          />
+        </div>
+        <div className="rounded-[40px] bg-white p-10 text-black shadow-xl ml-8">
+          <div className="flex justify-between items-center mb-4">
+            <h6>Welcome to <span className="font-bold">CITESCOUT</span></h6>
+            <span className="text-xs text-[#32324D]">
+              Already have an account? <a className="underline" href="/signin">Sign In</a>
+            </span>
+          </div>
+
+          <h2 className="text-[40px] font-semibold mb-8">Sign Up</h2>
+          <form onSubmit={handleSignupWithEmailAndPassword}>
+            <button
+              type="button"
+              onClick={handleGoogleSignIn}
+              className="flex items-center border border-gray-300 px-4 py-3 rounded-xl w-[50%] mt-5 text-black hover:bg-gray-100 focus:ring-blue-400 focus:outline-none"
+            >
+              <FcGoogle className="mr-2 text-xl" />
+              Sign in with Google
+            </button>
+            <div className="mt-5">
+              <label>Enter your email address</label>
+              <input
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+
+                type="email"
+                className="border border-gray-300 px-4 py-3 rounded-xl w-full focus:ring-blue-400"
+                placeholder="Email address"
+                required
+              />
+            </div>
+            <div className="mt-5">
+              <label>Create your Password</label>
+              <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+
+                className="border border-gray-300 px-4 py-3 rounded-xl w-full focus:ring-blue-400 focus-within:ring-blue-400 focus-within:outline-blue-400"
+                placeholder="password"
+                required
+             
+                />
+            </div>
+            <div className="mt-5">
+              <label>Confirm Password</label>
+              <input
+                type="password"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+                
+                className="border border-gray-300 px-4 py-3 rounded-xl w-full focus:ring-blue-400 focus-within:ring-blue-400 focus-within:outline-blue-400"
+                placeholder="confirm password"
+
+            />
+            </div>
+            <button
+              type="submit"
+              className="bg-[#615793] hover:bg-[#32324D] text-white font-bold py-4 px-3 rounded-xl w-full mt-8"
+            >
+              Sign Up
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+
+export default SignUp;
--- src/components/SideMenu.js
+++ src/components/SideMenu.js
@@ -1,11 +1,30 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { Dialog, DialogBackdrop, DialogPanel, DialogTitle } from '@headlessui/react'
+import { ExclamationTriangleIcon } from '@heroicons/react/24/outline'
+import { getAuth, signOut } from "firebase/auth";
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
 }
 
 export default function SideMenu() {
+  const [isOpen, setIsOpen] = useState(false);
+  const navigate = useNavigate();
+  const auth = getAuth();
+
+  const handleLogout = () => {
+    signOut(auth)
+      .then(() => {
+        console.log("Logged out");
+        setIsOpen(false);
+        navigate('/signin');
+      })
+      .catch((error) => {
+        console.error("Logout failed:", error);
+      });
+  };
+
   const navigation = [
     {
       name: "Home",
@@ -88,9 +107,8 @@ export default function SideMenu() {
     } else {
       nav.current = false
     }
-  }) 
-
-
+  });
+    
   return (
     <div className="fixed left-0 top-0 h-screen w-64 overflow-hidden bg-[#F9F6F9] transition-all duration-300">
       <div className="flex h-full flex-col p-5">
@@ -124,9 +142,9 @@ export default function SideMenu() {
         </nav>
 
         <div className="mt-auto pt-5">
-          <Link
-            to="/logout"
-            className={classNames(
+          <button 
+          onClick={() => setIsOpen(true)}
+          className={classNames(
               "flex items-center gap-4 whitespace-nowrap rounded-md p-3 font-medium",
               "fill-[#71839B] text-[#324054] hover:bg-purple-100/50 hover:text-purple-800",
             )}
@@ -146,12 +164,64 @@ export default function SideMenu() {
               />
             </svg>
             <p>Logout</p>
-          </Link>
+            </button>
         </div>
+        {/* <Example open={open} setOpen={setIsOpen} /> */}
+      </div>
+      <Dialog open={isOpen} onClose={() => setIsOpen(false)} className="relative z-10">
+    <DialogBackdrop
+      transition
+      className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in"
+    />
+
+    <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+      <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+        <DialogPanel
+          transition
+          className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all data-[closed]:translate-y-4 data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in sm:my-8 sm:w-full sm:max-w-lg data-[closed]:sm:translate-y-0 data-[closed]:sm:scale-95"
+        >
+          <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+            <div className="sm:flex sm:items-start">
+              <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                <ExclamationTriangleIcon aria-hidden="true" className="h-6 w-6 text-red-600" />
+              </div>
+              <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                <DialogTitle as="h3" className="text-base font-semibold leading-6 text-gray-900">
+                  Confirm Logout
+                </DialogTitle>
+                <div className="mt-2">
+                  <p className="text-sm text-gray-500">
+                    Are you sure you want to logout? Any unsaved changes will be lost.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+            <button
+              type="button"
+              onClick={handleLogout}
+              className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
+            >
+              Continue Logout
+            </button>
+            <button
+              type="button"
+              data-autofocus
+              onClick={() => setIsOpen(false)}
+              className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+            >
+              Cancel
+            </button>
+          </div>
+        </DialogPanel>
       </div>
     </div>
-  );
+  </Dialog>
+  </div>
+);
 }
+
 
 function LogoMark() {
   return (
